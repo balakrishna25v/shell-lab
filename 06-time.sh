dfead39aa9ecb021bef0c1526a5b576cd47f73c7
balu <balu.v25@gmail.com>
2026-02-09 01:00:38 +0530

    action

--- 06-time.sh
+++ 06-time.sh
@@ -2,3 +2,13 @@
 
 Timestamp=$(date)
 echo "script executed at: $Timestamp"
+
+
+sleep 10
+
+enD_TIME=$(date +%S)
+tOTAL_TIME=$(($enD_TIME - $SECONDS))
+
+
+
+echo "total execution time: $tOTAL_TIME"

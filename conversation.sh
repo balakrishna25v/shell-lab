80793600a32654a3e9b8639e46d092691d209ec3
balu <balu.v25@gmail.com>
2026-02-08 21:04:47 +0530

    action

--- conversation.sh
+++ conversation.sh
@@ -1,2 +1,7 @@
-hello world here
-Hope every one doing good and fine
+#!/bin/bash
+
+echo "Balu, System!"
+echo "Welcome to the world of scripting."
+echo "Welcome to the script to demonstrate how to create and run a script in the terminal."
+echo "Feel free to modify and welcome to this script and explore more about bash scripting!"
+echo "Happy scripting!"
---
+++ hello-system.sh
@@ -0,0 +1,7 @@
+#!/bin/bash\
+# A simple script to print "Hello, World!" to the console
+
+echo "Hello, system welcome! to the world of scripting."
+
+
+echo "This is a simple bash script to demonstrate how to create and run a script in the terminal."

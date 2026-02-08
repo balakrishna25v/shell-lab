784b7a260c40b05cf653440c2800293fcadc75b1
balu <balu.v25@gmail.com>
2026-02-09 00:16:20 +0530

    action

--- variable1.sh
+++ variable1.sh
@@ -5,3 +5,10 @@ echo "Please enter your username:"
 read user_name
 
 echo "user_name is $user_name"
+
+echo "enter your password:"
+read -s password
+
+echo " your password is $password"
+
+echo "your successfully logged in with username: $user_name and password: $password"

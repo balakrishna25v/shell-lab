02deaa76112c33f885f2ef4b58f536eb23409fc2
balu <balu.v25@gmail.com>
2026-02-08 21:06:48 +0530

    action

---
+++ variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+person1="Suresh" # no spaces allowed in variable names
+person2="ramesh"
+
+echo "$person1:: Hello $person2 how are you doing?"
+echo "$person2:: I am doing good $person1, how about you?"
+echo "$person1:: I am doing good as well, thanks for asking!"
+echo "$person2:: That's great to hear! Do you have any plans for the weekend?"
+echo "$person1:: Yes, I am planning to go hiking with some friends. How about you?"
+echo "$person2:: That sounds fun! I am planning to relax at home and catch up on some reading."
+echo "$person1:: That sounds like a great way to spend the weekend

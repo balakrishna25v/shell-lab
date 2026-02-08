f74c3dd4de5c4256b5547ecf37ca51f03d020b41
balu <balu.v25@gmail.com>
2026-02-08 20:47:32 +0530

    test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Suresh:: Hello  ramesh how are you doing?"
+echo "Ramesh:: I am doing good suresh, how about you?" 
+echo "Suresh:: I am doing good as well, thanks for asking!"
+echo "Ramesh:: That's great to hear! Do you have any plans for the weekend?"
+echo "Suresh:: Yes, I am planning to go hiking with some friends. How about you?"
+echo "Ramesh:: That sounds fun! I am planning to relax at home and catch up on some reading."
+echo "Suresh:: That sounds like a great way to spend the weekend. Enjoy your reading!"
+echo "Ramesh:: Thanks, you enjoy your hiking as well! Have a great weekend!"
+echo "Suresh:: You too, Ramesh! Take care and have a wonderful weekend!"

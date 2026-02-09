f86e727f6c04cc9d32348baf220a17b90ecc9a2d
balu <balu.v25@gmail.com>
2026-02-10 01:32:12 +0530

    action

--- 09-new4.sh
+++ 09-new4.sh
@@ -8,3 +8,10 @@ SUM=$(($Num1 + $Num2))
 echo "Sum of is: $SUM"
 
 echo "Happy Day to you"
+
+
+FRUITS=("Apple" "Banana" "Orange")
+
+echo "First fruit is: ${FRUITS[@]}"
+echo "Second fruit is: ${FRUITS[0]}"
+echo "Third fruit is: ${FRUITS[1]}"     

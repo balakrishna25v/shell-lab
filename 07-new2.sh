70a5595ef567ab438193475f31cc9c6673b7e828
balu <balu.v25@gmail.com>
2026-02-09 21:35:05 +0530

    action

--- 07-new2.sh
+++ 07-new2.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+
+userid=$(id -u)
+if [ $userid -ne 0 ]; then
+    echo "You are running this script as root."
+else
+    echo "You are running this script as a regular user."
+fi
+
+echo "user ID is $userid and print a message."\
--- 08-new3.sh
+++ 08-new3.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+echo "all args passed to the script: $@"
+echo "Number of vars passed to the script: $#"
+echo "script name is: $0"
+echo "present working directory is: $PWD"
+echo "who is running this script: $USER"
+echo "home directory of the user is: $HOME"
+echo "PID of the current script is: $$"
+sleep 100&
+echo "PID of recently executed background process is: $!"
+echo "All args passed to script is: $*"

f280877032997d2169200cd5304e68d28b757d27
balu <balu.v25@gmail.com>
2026-02-10 01:38:32 +0530

    action

--- 10-new5.sh
+++ 10-new5.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+NUMBER=25
+
+
+# -gt is greater than
+# -lt is less than
+# -eq is equal to
+# -ne is not equal to
+# -ge is greater than or equal to
+# -le is less than or equal to  
+
+
+if [ $NUMBER -gt 20 ]; then
+    echo "The number is greater than 20."
+else
+    echo "The number is less than or equal to 20."
+fi

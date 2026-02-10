07da3fd5b5336766719bf0631011d32b4d5dc361
balu <balu.v25@gmail.com>
2026-02-10 13:12:49 +0530

    action

--- 11-new5.sh
+++ 11-new5.sh
@@ -4,9 +4,6 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
     echo "You are running this script as root."
     exit 1
-else
-    echo "You are running this script as a regular user."
-    exit 0
 fi
 
 echo "installing nginx"

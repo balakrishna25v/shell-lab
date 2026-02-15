7434dc2013e0a071c40da0180e66f7305d7a00f5
balu <balu.v25@gmail.com>
2026-02-15 17:06:26 +0530

    action

---
+++ 16-new.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script  root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+# Create logs directory if it doesn't exist
+mkdir -p $LOGS_FOLDER
+
+VALIDATE()
+{ 
+    if [ $1 -ne 0 ]; then
+        echo "$2 ....failed" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ....success" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ # Loop through each package name provided as an argument
+do
+    dnf install $package -y  # Install the package and redirect output to the log file
+    VALIDATE $? "$package installation" # Validate the installation of
+
+done

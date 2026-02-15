87af70dcba142da776c842b815337c32df53f474
balu <balu.v25@gmail.com>
2026-02-15 11:17:00 +0530

    action

--- 15-new.sh
+++ 15-new.sh
@@ -15,10 +15,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){ 
     if [ $1 -ne 0 ]; then
-        echo "$2 ....failed" | tee -a $LOGS_FILE
+        echo "$1 ....failed" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2 ....success" | tee -a $LOGS_FILE
+        echo "$1 ....success" | tee -a $LOGS_FILE
     fi
 }
 

de1310847bc05f458f56334563b2c5b282ce6272
balu <balu.v25@gmail.com>
2026-02-15 10:54:21 +0530

    action

---
+++ 15-new.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script  root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+# Create logs directory if it doesn't exist
+mkdir -p $LOGS_FOLDER
+
+
+
+VALIDATE(){ 
+    if [ $1 -ne 0 ]; then
+        echo "$2 ....failed" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ....success" | tee -a $LOGS_FILE
+    fi
+}
+
+# Update the system
+dnf installing nginx -y &>> $LOGS_FILE
+VALIDATE $? "Nginx installing"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "MySQL installing"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "nodejs installing"

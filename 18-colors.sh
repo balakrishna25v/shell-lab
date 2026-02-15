f83659d1326dd4abf200e1e80f1809a3648543b5
balu <balu.v25@gmail.com>
2026-02-15 17:29:36 +0530

    action

---
+++ 18-colors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+Y="\e[33m" # Yellow color
+B="\e[34m" # Blue color
+M="\e[35m" # Magenta color
+C="\e[36m" # Cyan color
+W="\e[37m" # White color
+
+
+echo -e "$R Hello world $N, $Y I am learning Linux $N "

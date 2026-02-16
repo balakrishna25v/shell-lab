2df7dcbbca28ac97e5de37edc02c3b0e9ae782c7
balu <balu.v25@gmail.com>
2026-02-16 18:55:31 +0530

    action

--- 20-trap.sh
+++ 20-trap.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
 
-set -e #ERR
+set -e 
 
-trap 'echo "there is an error in $LINENO   , Command : $BASH_COMMAND..." ERR
+trap 'echo "there is an error in $LINENO, Command : $BASH_COMMAND..."' ERR
 
-echo "Hello world!."
-echo "I am learning shell."
-echo "printing error here"
-echo "No error in the"
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script  root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+# Create logs directory if it doesn't exist
+mkdir -p $LOGS_FOLDER
+
+
+for package in $@ # Loop through each package name provided as an argument
+do
+dnf list installed $package &>> $LOGS_FILE #Check if the package is already installed and redirect output to the log file   
+if [ $? -ne 0 ]; then # Check if the package is not installed
+    echo "$package is not installed. Installing now..." | tee -a $LOGS_FILE # Log the message about installing the package
+    dnf install $package -y  &>> $LOGS_FILE # Install the package and redirect output to the log file
+   # VALIDATE $? "$package installation" # Validate the installation of the package
+else
+    echo "$package is already installed. Skipping installation." | tee -a $LOGS_FILE
+fi
+done
